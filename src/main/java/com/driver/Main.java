4bb9394771f1e0f2056033e2be9ce65a8fe4aaf8
S-APNA95 <sapna.acharya3@gmail.com>
2024-02-05 17:37:05 +0530

    updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,7 @@
 package com.driver;
 
-public class A {
-    public String meth(){
-        return "Invoking method from class A";
 
-    }
 
-}
-public class B extends A{
-    @Override
-    public String meth(){
-        return "Method is overridden in Extended class B";
-
-    }
-
-}
 
 public class Main {
     public static void main(String[] args) {
